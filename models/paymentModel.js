5e052f3635f735123b9e71ed29c13badff177799
ayushvadodariya <ayushvadodariya12@gamil.com>
2024-06-23 13:06:17 +0530

    Add payment feature using Razorpay integration

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose");
 const paymentSchema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  amount: Number,
-  date: { type: Date, default: Date.now },
-  status: String,
-});
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  payer_name: {
+    type: String
+  },
+  amount: {
+    type: Number,
+    default: 0
+  },
+  razorpay_order_id: {
+    type: String,
+    default:null
+  },
+  razorpay_payment_id: {
+    type: String,
+    default: null
+  },
+  status: {
+    type: String,
+    default: "created"
+  },
+}, { timestamps: true});
 module.exports = mongoose.model("Payment", paymentSchema);
